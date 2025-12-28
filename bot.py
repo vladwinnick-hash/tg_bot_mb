250a40f788e28629db026adc87e237db98dfe681
vladwinnick-hash <vlad.winnick@gmail.com>
2025-12-29 02:06:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = ''
+TOKEN = '8523112202:AAEaoiDoiHIHQV0405w_Ppah38LiLpXNVdU'
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
